bc2d993e53e535ea27ae14e56f71da159011e815
Liam-McCartney <98416389+Liam-McCartney@users.noreply.github.com>
2024-06-05 22:14:23 -0400

    Add files via upload

---
+++ Rocket Telemetry/rocketTelemetryCompSide.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC12(10,11);
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);
+
+}
+
+void loop() {
+  while (HC12.available()) {
+    Serial.write(HC12.read());
+  }
+  while (Serial.available()) {
+    HC12.println("Serial.read()");
+  }
+}
